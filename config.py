d65142835f6c2683480dd74c9ecb014695dac553
averyboy <averyboy@qq.com>
2018-03-06 22:00:59 +0800

    add config

---
+++ config.py
@@ -0,0 +1,14 @@
+class mail_config:
+    MAIL_SERVER='smtp.qq.com'
+    MAIL_PORT=465
+    MAIL_USE_SSL=True
+    MAIL_USERNAME='228744592@qq.com'
+    MAIL_PASSWORD='iiyopczqdflpbghe'
+    MAIL_DEFAULT_SENDER='averyboy@qq.com'
+
+class db_config:
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:qq228744592@localhost/test'
+    SQLAlCHEMY_COMMIT_TEARDOWN=True
+    
+
+
