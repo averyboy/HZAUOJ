e62d9d333d424e5651d187e7f555a4d9f0283c06
averyboy <averyboy@qq.com>
2018-03-07 20:03:21 +0800

    update

---
+++ config_model.py
@@ -0,0 +1,18 @@
+class app_config:
+    MAIL_SERVER=
+    MAIL_PORT=
+    MAIL_USE_SSL=
+    MAIL_USERNAME=
+    MAIL_PASSWORD=
+    MAIL_DEFAULT_SENDER=
+    SQLALCHEMY_DATABASE_URI=
+    SQLAlCHEMY_COMMIT_TEARDOWN=
+    SECRET_KEY=
+config={
+    'app':app_config
+    'test':app_config
+    'default':app_config
+}
+    
+
+
